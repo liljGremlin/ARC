1d7f5910c769c27efd5e51483be108a6b4a3a902
Brandon Castro <bcastro9@ucsc.edu>
2023-02-24 02:52:46 -0800

    finished mobile css

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+var navChange = false;
+var elementNav = document.getElementById("navbar");
+
+window.addEventListener("scroll", function() {
+    var elementTarget = document.getElementById("home");
+    if (window.scrollY > (elementTarget.offsetTop + elementTarget.offsetHeight)) {
+        
+        console.log(elementNav.getAttribute);
+    }
+
+    if (window.scrollY < (elementTarget.offsetTop + elementTarget.offsetHeight))
+    {
+        console.log("a");
+    }
+
+    // if (!navChange)
+    // {
+
+    //     elementNav.style.visibility = 'hidden';
+    //     navChange = false;
+    // }
+  });
+  */
